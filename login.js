6d276885aa2556350c919cd8df1e229a24edfbde
kamiakhs <admin@kamiak.org>
2022-08-25 10:09:00 -0700

    a

--- login.js
+++ login.js
@@ -21,29 +21,24 @@ provider.setCustomParameters({
 const auth = getAuth(app);
 let user = null;
 
-signInWithPopup(auth, provider)
+const login = () => signInWithPopup(auth, provider)
   .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
     const credential = GoogleAuthProvider.credentialFromResult(result);
     const token = credential.accessToken;
-    // The signed-in user info.
     user = result.user;
-    // ...
-    // console.log(user);
   }).catch((error) => {
-    // Handle Errors here.
     const errorCode = error.code;
     const errorMessage = error.message;
-    // The email of the user's account used.
-    // const email = error.customData.email;
-    // The AuthCredential type that was used.
     const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
     console.log(error.code);
     console.log(error.message);
   })
 ;
 
 onAuthStateChanged(auth, user => {
-  console.log(user);
+  if (user) {
+    console.log(user);
+  } else {
+    login();
+  }
 });
