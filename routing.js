493bbe980c39f9bdca7933f8c4923685a0ccfcd0
jwseph <kodomo.oishii@gmail.com>
2022-12-24 13:11:51 -0800

    a

--- routing.js
+++ routing.js
@@ -5,98 +5,99 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     mapTypeControl: false,
-    center: { lat: -33.8688, lng: 151.2195 },
+    center: { lat: 47.900769, lng: -122.301757 },
     zoom: 13,
   });
 
+  window.map = map;
   window.handler = new AutocompleteDirectionsHandler(map);
 }
 
 class AutocompleteDirectionsHandler {
   map;
-  placeIds;
+  originPlaceId;
+  destinationPlaceId;
+  waypointPlaceIds;
   travelMode;
   directionsService;
   directionsRenderer;
   constructor(map) {
     this.map = map;
-    this.placeIds = [];
+    this.originPlaceId = '';
+    this.destinationPlaceId = '';
+    this.waypointPlaceIds = [];
     this.travelMode = google.maps.TravelMode.WALKING;
     this.directionsService = new google.maps.DirectionsService();
     this.directionsRenderer = new google.maps.DirectionsRenderer();
     this.directionsRenderer.setMap(map);
+    
+    let modes = [
+      {id: 'changeModeWalking', enum: google.maps.TravelMode.WALKING},
+      {id: 'changeModeBiking', enum: google.maps.TravelMode.BICYCLING},
+      {id: 'changeModeTransit', enum: google.maps.TravelMode.TRANSIT},
+      {id: 'changeModeDriving', enum: google.maps.TravelMode.DRIVING},
+    ];
+    for (const mode of modes) {
+      const radioButton = document.getElementById(mode.id);
+      radioButton.addEventListener("click", () => {
+        this.travelMode = mode.enum;
+        this.route();
+      });
+    }
 
-    const modeSelector = document.getElementById("mode-selector");
+    this.addAutocomplete($('#originInput')[0], (place) => {
+      this.originPlaceId = place.place_id;
+    });
 
-    this.setupClickListener(
-      "changemode-walking",
-      google.maps.TravelMode.WALKING
-    );
-    this.setupClickListener(
-      "changemode-transit",
-      google.maps.TravelMode.TRANSIT
-    );
-    this.setupClickListener(
-      "changemode-driving",
-      google.maps.TravelMode.DRIVING
-    );
-    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
-    this.addInput();
-    this.addInput();
-  }
-  addInput() {
-    let i = this.placeIds.length;
-    this.placeIds.push(null);
-    let input = document.createElement('input');
-    input.classList.add('controls', 'place-input');
-    input.type = 'text';
-    input.placeholder = 'Enter a location';
-    let autocomplete = new google.maps.places.Autocomplete(
-      input,
-      { fields: ["place_id"] }
-    );
-    this.setupPlaceChangedListener(autocomplete, i);
-    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-  }
-  // Sets a listener on a radio button to change the filter type on Places
-  // Autocomplete.
-  setupClickListener(id, mode) {
-    const radioButton = document.getElementById(id);
-
-    radioButton.addEventListener("click", () => {
-      this.travelMode = mode;
-      this.route();
+    this.addAutocomplete($('#destinationInput')[0], (place) => {
+      this.destinationPlaceId = place.place_id;
     });
   }
-  setupPlaceChangedListener(autocomplete, i) {
-    autocomplete.bindTo("bounds", this.map);
-    autocomplete.addListener("place_changed", () => {
-      const place = autocomplete.getPlace();
-
+  addWaypoint() {
+    this.waypointPlaceIds.push('');
+    let stop = makeStop();
+    let input = stop.querySelector('input');
+    this.addAutocomplete(input, (place) => {
+      let i = getStopIndex(stop);
+      console.log(i);
+      this.waypointPlaceIds[i] = place.place_id;
+    });
+    addStop(stop);
+  }
+  removeWaypoint(stop) {
+    console.log(stop);
+    let i = getStopIndex(stop);
+    removeStop(stop);
+    this.waypointPlaceIds.splice(i, 1);
+    this.route();
+  }
+  addAutocomplete(input, callback) {
+    let autocomplete = new google.maps.places.Autocomplete(input, {fields: ['place_id']});
+    autocomplete.bindTo('bounds', map);
+    autocomplete.addListener('place_changed', () => {
+      let place = autocomplete.getPlace();
       if (!place.place_id) {
         window.alert("Please select an option from the dropdown list.");
         return;
       }
-
-      console.log('yeet '+i);
-      this.placeIds[i] = place.place_id;
+      console.log(autocomplete);
+      window.autocomplete = autocomplete;
+      callback(place);
       this.route();
     });
   }
   route() {
-    for (const placeId of this.placeIds) {
-      if (!placeId) return;
+    if (!this.originPlaceId || !this.destinationPlaceId) return;
+    let waypoints = [];
+    for (const waypointPlaceId of this.waypointPlaceIds) {
+      if (!waypointPlaceId) return;
+      waypoints.push({location: {placeId: waypointPlaceId}, stopover: true});
     }
     
-    let waypoints = [];
-    for (let i = 1; i < this.placeIds.length-1; i++) {
-      waypoints.push({location: {placeId: this.placeIds[i]}, stopover: true});
-    }
-    console.log(this.placeIds[0], waypoints, this.placeIds[this.placeIds.length-1]);
     this.directionsService.route(
       {
-        origin: { placeId: this.placeIds[0] },
-        destination: { placeId: this.placeIds[this.placeIds.length-1] },
+        origin: { placeId: this.originPlaceId },
+        destination: { placeId: this.destinationPlaceId },
         travelMode: this.travelMode,
         waypoints: waypoints,
         optimizeWaypoints: true,
@@ -119,7 +120,8 @@ window.initMap = initMap;
 
 // Dragbar handling
 let $dragLeft = $('#dragLeft');
-let dragOffset;
+window.dragOffset = null;
+// let dragOffset;
 const drag = (e) => {
   // Remove current selection
   if (document.selection) {
@@ -139,5 +141,39 @@ $(document).on('mouseup', () => {
   if (dragOffset == null) return;
   dragOffset = null;
   $('html, body').css('cursor', '');
-  $(document).on('mousemove', drag);
+  $(document).off('mousemove', drag);
 });
+
+const makeStop = () => {
+  let $stop = $(`
+    <div class='row mainStart crossCenter fullWidth'>
+      <i class="material-icons-outlined spaceAroundIcon">place</i>
+      <label class="expand dense mdc-text-field mdc-text-field--outlined mdc-text-field--no-label">
+        <span class="mdc-notched-outline">
+          <span class="mdc-notched-outline__leading"></span>
+          <span class="mdc-notched-outline__trailing"></span>
+        </span>
+        <input class="mdc-text-field__input" type="text" placeholder='Enter stop' aria-label="Label">
+      </label>
+      <button class="mdc-icon-button material-icons-outlined" onclick="handler.removeWaypoint(this.parentNode);">
+        <div class="mdc-icon-button__ripple"></div>
+        delete
+      </button>
+    </div>
+  `);
+  new mdc.textField.MDCTextField($stop.find('.mdc-text-field')[0]);
+  $stop.find('.mdc-icon-button').each(function () {
+    let iconButtonRipple = new mdc.ripple.MDCRipple(this);
+    iconButtonRipple.unbounded = true;
+  });
+  return $stop[0];
+}
+const addStop = (stop) => $('#stops').append(stop);
+const removeStop = (stop) => $(stop).remove();
+const getStopIndex = (stop) => Array.from($('#stops')[0].children).indexOf(stop);
+
+
+$('.mdc-radio').each(function () { new mdc.radio.MDCRadio(this); });
+$('.mdc-form-field').each(function () { new mdc.formField.MDCFormField(this); });
+$('.mdc-button').each(function () { new mdc.ripple.MDCRipple(this); });
+$('.mdc-text-field').each(function () { new mdc.textField.MDCTextField(this); });
